ac78834f09e7488a9a00ad0bd471e164ac7fca0b
voracious-accelerator-8615 <margo.reiss.gold@gmail.com>
2020-05-25 18:00:53 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  # board is an array containing values
-  # corresponding to moves on the board
-  puts " #{board[0]}  | #{board[1]}   | #{board[2]}  "
+  puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
